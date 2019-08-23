a5c109321d0c18612a2a1248ecc733f92bfc96af
Thiago Dantas <thiagodma52@gmail.com>
2019-08-23 08:49:40 -0300

    Main modificado para estar em padrão com a classe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from classic_clustering import ClassicClustering
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from scipy.cluster import hierarchy
+
 import pandas as pd
 import time
 import matplotlib.pyplot as plt
@@ -18,28 +17,22 @@ cc.importa_textos()
 cc.stem()
 
 #Vetorizando e aplicando o tfidf
-vec = CountVectorizer()
-bag_palavras = vec.fit_transform(cc.textos_stem)
-feature_names = vec.get_feature_names()
-base_tfidf = TfidfTransformer().fit_transform(bag_palavras)
-base_tfidf = base_tfidf.todense()
+cc.vec_tfidf()
 
 #Reduzindo a dimensionalidade. Você deve definir o número de dimensões da saída.
 #Procure um valor que garanta pelo menos 85% da variância explicada
 n_dims = 600
 base_tfidf_reduced = cc.SVD(n_dims, base_tfidf)
 
-
 #Clustering
 print('Começou a clusterização.')
 t = time.time()
-clusters_por_cosseno = hierarchy.linkage(base_tfidf_reduced,"average", metric="cosine") #pode testar metric="euclidean" também
+clusters_por_cosseno = hierarchy.linkage(base_tfidf_reduced,"average", metric="cosine")
 plt.figure()
 dn = hierarchy.dendrogram(clusters_por_cosseno)
 
 #Você deve alterar o limite de dissimilaridade da forma que melhor se ajuste ao seu dendograma
 limite_dissimilaridade = 0.92
-
 id_clusters = hierarchy.fcluster(clusters_por_cosseno, limite_dissimilaridade, criterion="distance")
 elpsd = time.time() - t
 print('Tempo para fazer a clusterização: ' + str(elpsd) + '\n')
